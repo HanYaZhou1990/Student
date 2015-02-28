ffdd02c570e7acf296dcfa495922dc131589e96a
wwr <925992894@qq.com>
2015-03-01 00:03:25 +0800

    增加通知界面

---
+++ AmoyMasterStudents/AmoyMasterStudents/ViewControllers/NoticeViewController.h
@@ -0,0 +1,15 @@
+//
+//  NoticeViewController.h
+//  AmoyMasterStudents
+//
+//  Created by julong on 15/2/28.
+//  Copyright (c) 2015年 renbing. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface NoticeViewController : BaseViewController
+
+@property(nonatomic,strong) NSString *noticeType;
+
+@end
